1de2f7ea00459134a9acb87619989f552116ef06
Patrik Fridström <frizze18@gmail.com>
2017-11-27 12:00:52 +0100

    did important clever changes

---
+++ js-app/data.js
@@ -0,0 +1,10 @@
+let movieData ={
+
+    title:"Goon",
+    rewiev:"   A Pretty good hockey movie that capture the life of a enforcer.As many doesn't know a enforcer in hockey is all about figthing and standing up for his teammates.",
+    cast:  ["Seann William", 
+            "Jay Baruchel", 
+            "Liev Schreiber"
+            ],
+    image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTcxOTQwMTQ3MF5BMl5BanBnXkFtZTcwMDcyOTQwNw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+};
