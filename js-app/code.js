8c310eab5eb0a3be2e722ab12a0bb6e713fd4f64
Patrik Fridström <frizze18@gmail.com>
2017-11-27 16:53:53 +0100

    change js to jQuery and made an package.jason file.

--- js-app/code.js
+++ js-app/code.js
@@ -1,33 +1,57 @@
 function renderMovie(data){
-    document.querySelector(".header").innerText=data.title;
-    document.querySelector("#rewiev").innerText=data.rewiev;
+    //document.querySelector(".header").innerText=data.title;
+    //$(".header").text(data.title);
+    ui.title.text(data.title);
 
-    document.querySelector(".img").setAttribute("src",data.image);
+   // document.querySelector("#rewiev").innerText=data.rewiev;
+   //$("#rewiev").text(data.rewiev);
+   ui.rewiev.text(data.rewiev);
+
+   // document.querySelector(".img").setAttribute("src",data.image);
+   $(".img").attr("src",data.image);
+   //ui.image
+
 
     let actorlist="";
     for (let i = 0; i < data.cast.length; i++) {
         actorlist += "<li>" + data.cast[i] +"</li>";
     }
-    document.querySelector(".cast").innerHTML = actorlist;
+  //  document.querySelector(".cast").innerHTML = actorlist;
+    $(".cast").html(actorlist);
 }
 
 
 
 function changeStarRating(grade) {
-    //Update stars so they appear filled til the number it has been rated to.
     for (let i = 0; i < 5; i++) {
-        let star = document.getElementById("star"+i);
+        let star = $("#star" + i); //document.getElementById("star"+i);
         if(i<=grade){
-            star.classList.add("filledstars");
+            //star.classList.add("filledstars");
+            star.addClass("filledstars");
         }else{
-            star.classList.remove("filledstars");
+           // star.classList.remove("filledstars");
+            star.removeClass("filledstars");
         }
-    }  
+    }
+
+    /*
+    let collectionStars = $(".stars");
+    collectionStars.addClass("filledstars");
+    */
 }
 for(let i=0; i<5;i++){
-    let star = document.getElementById("star"+i).addEventListener("click",function(){
+   /* let star = document.getElementById("star"+i).addEventListener("click",function(){
+        changeStarRating(i);
+    });*/
+    $("#star"+i).click(function(){
+        console.log("WOOT")
         changeStarRating(i);
     });
+}
+let ui={
+    title:$(".header"),
+    rewiev:$("#rewiev"),
+    image:$(".img")
 }
     
 //changeStarRating(3);
