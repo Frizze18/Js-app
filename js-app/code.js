a81dc3bade09a8d3a91dbbb09bdcc43a4f4a1f98
Patrik Fridström <frizze18@gmail.com>
2017-11-27 14:38:53 +0100

    added star rating

--- js-app/code.js
+++ js-app/code.js
@@ -11,4 +11,24 @@ function renderMovie(data){
     document.querySelector(".cast").innerHTML = actorlist;
 }
 
+
+
+function changeStarRating(grade) {
+    //Update stars so they appear filled til the number it has been rated to.
+    for (let i = 0; i < 5; i++) {
+        let star = document.getElementById("star"+i);
+        if(i<=grade){
+            star.classList.add("filledstars");
+        }else{
+            star.classList.remove("filledstars");
+        }
+    }  
+}
+for(let i=0; i<5;i++){
+    let star = document.getElementById("star"+i).addEventListener("click",function(){
+        changeStarRating(i);
+    });
+}
+    
+//changeStarRating(3);
 renderMovie(movieData);
